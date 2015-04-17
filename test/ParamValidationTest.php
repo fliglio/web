3e9c63d97c91af4e92458dccb486b37401db8ea1
Ben Schwartz <benschw@gmail.com>
2015-04-17 09:41:54 -0500

    adding IntParams

--- test/ParamValidationTest.php
+++ test/ParamValidationTest.php
@@ -6,6 +6,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class ParamValidationTest extends \PHPUnit_Framework_TestCase {
 
+	//=========================================================================
+	// Param
+	//
+
 	public function testParamShouldValidateWithNoConstraints() {
 		// given
 		$param = new Param("foo");
@@ -42,5 +46,118 @@ class ParamValidationTest extends \PHPUnit_Framework_TestCase {
 		$foo = $param->get();
 	}
 
+	//=========================================================================
+	// IntParam
+	//
+
+	public function testIntParamShouldValidateIntegerValue() {
+		// given
+		$param = new IntParam(123);
+		
+		// when
+		$param->validate();
+		
+		// then
+		$this->assertEquals(123, $param->get());
+	}
+
+	/**
+	 * @expectedException Fliglio\Web\ValidationException
+	 */
+	public function testBadRequestThrownWhenValidatingInvalidIntParam() {
+		// given
+		$param = new IntParam("foo");
+
+		// when
+		$param->validate();
+	}
+
+	/**
+	 * @expectedException Fliglio\Http\Exceptions\BadRequestException
+	 */
+	public function testBadRequestThrownWhenGettingInvalidIntParam() {
+
+		// given
+		$param = new IntParam("foo");
+
+		// when
+		$foo = $param->get();
+	}
+
+	//=========================================================================
+	// IntGetParam
+	//
+
+	public function testIntGetParamShouldValidateIntegerValue() {
+		// given
+		$param = new IntGetParam(123);
+		
+		// when
+		$param->validate();
+		
+		// then
+		$this->assertEquals(123, $param->get());
+	}
+
+	/**
+	 * @expectedException Fliglio\Web\ValidationException
+	 */
+	public function testBadRequestThrownWhenValidatingInvalidIntGetParam() {
+		// given
+		$param = new IntGetParam("foo");
+
+		// when
+		$param->validate();
+	}
+
+	/**
+	 * @expectedException Fliglio\Http\Exceptions\BadRequestException
+	 */
+	public function testBadRequestThrownWhenGettingInvalidIntGetParam() {
+
+		// given
+		$param = new IntGetParam("foo");
+
+		// when
+		$foo = $param->get();
+	}
+
+	//=========================================================================
+	// IntPathParam
+	//
+
+	public function testIntPathParamShouldValidateIntegerValue() {
+		// given
+		$param = new IntPathParam(123);
+		
+		// when
+		$param->validate();
+		
+		// then
+		$this->assertEquals(123, $param->get());
+	}
+
+	/**
+	 * @expectedException Fliglio\Web\ValidationException
+	 */
+	public function testBadRequestThrownWhenValidatingInvalidIntPathParam() {
+		// given
+		$param = new IntPathParam("foo");
+
+		// when
+		$param->validate();
+	}
+
+	/**
+	 * @expectedException Fliglio\Http\Exceptions\BadRequestException
+	 */
+	public function testBadRequestThrownWhenGettingInvalidIntPathParam() {
+
+		// given
+		$param = new IntPathParam("foo");
+
+		// when
+		$foo = $param->get();
+	}
 
 }
