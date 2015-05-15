33a74b507051b8408fd592c0ae548c0e05334ce7
Philip W <philip.whitt@sbcglobal.net>
2015-05-15 09:41:23 -0500

    Updating Int Param validator to use regex, to allow for string ints

--- Fliglio/Web/IntParam.php
+++ Fliglio/Web/IntParam.php
@@ -8,9 +8,9 @@ class IntParam extends Param {
 	public function __construct($val) {
 		parent::__construct($val);
 
-		$this->addConstraint(new Assert\Type(array(
-            'type'    => 'integer',
-            'message' => 'The value {{ value }} is not a valid {{ type }}.'
-        )));
+		$this->addConstraint(new Assert\Regex([
+            'pattern' => '/^[-+]?\d+$/',
+            'message' => 'The value {{ value }} is not a valid integer.'
+        ]));
 	}
 }
