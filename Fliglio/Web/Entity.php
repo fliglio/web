8b5d0cbd676046996f7cb42452adb2834254fbe3
david byrd <david.byrd@bancvue.com>
2017-07-19 16:05:46 -0500

    adding getcontenttype and test

--- Fliglio/Web/Entity.php
+++ Fliglio/Web/Entity.php
@@ -6,6 +6,7 @@ use Fliglio\Http\Exceptions\BadRequestException;
 
 class Entity {
 	private $body;
+	private $contentType;
 
 	public function __construct($body, $contentType) {
 		$this->body = $body;
@@ -14,11 +15,14 @@ class Entity {
 	public function get() {
 		return $this->body;
 	}
+	public function getContentType() {
+		return $this->contentType;
+	}
 	public function bind($entityType) {
 		if (!in_array('Fliglio\Web\MappableApi', class_implements($entityType))) {
 			throw new \Exception($entityType . " doesn't implement Fliglio\Web\MappableApi");
 		}
-		
+
 		$arr = null;
 		switch($this->contentType) {
 
